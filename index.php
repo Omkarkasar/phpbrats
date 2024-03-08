9ac70af11d37a0e7485b7b4b3dd0f7286f722afe
Omkarkasar <68294039+Omkarkasar@users.noreply.github.com>
2024-03-08 14:28:00 +0000

    This is new changes pls task1

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <php
-$servername=localhost;
+$servername="localhost";
+$username="root";
+$password="";
+$dbName="PHPCRUD";
+
+
+$conn=@mysqli_connect($servername,$username,$password,$dbName)
+if(conn)
+{
+    echo "connection is succesful";
+}
+else{
+ echo "connection is unsuccesful";
+}
+
 
 
 >
